73a3ac38a586b61badc12f810251be11e39ed48f
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-08-18 02:18:14 +0530

    updated works section

--- components/work/Work.jsx
+++ components/work/Work.jsx
@@ -1,69 +1,16 @@
 "use client";
 import Image from "next/image";
 import dummy from "../../public/dummywork.png";
+import one from "../../public/one.png";
+import two from "../../public/two.png";
+import three from "../../public/three.png";
+import four from "../../public/four.png";
 import { useState } from "react";
 import { Badge } from "../ui/badge";
 import Link from "next/link";
-import { GitHubLogoIcon, GlobeIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, GlobeIcon, StackIcon } from "@radix-ui/react-icons";
 function Work() {
   const [projects, setProjects] = useState([
-    {
-      name: "GuruCodes",
-      description:
-        "A high-converting course landing page, attracting over 100 daily visitors and facilitating 300+ course sales. Integrated Razorpay for secure transactions, directly contributing to revenue generation. Deployed and managed the application on AWS EC2, enhancing brand consistency and user trust.",
-      source: "",
-      live: "",
-      hassource: true,
-      islive: true,
-      start: "",
-      end: "",
-      technology: ["nextjs", "razorpay", "magicui", "tailwind", "analytics"],
-      ongoing: false,
-      img: "",
-    },
-    {
-      name: "Cs One Stop",
-      description:
-        "Optimized database models for 1000+ records using PrismaORM. Deployed NextAuth for authentication and added a portfolio interface. Integrated Judge0 IDE for real-time code execution, enhancing learning for 100+ students. Engineered a web scraping solution for job notifications.",
-      source: "",
-      live: "",
-      start: "",
-      end: "",
-      hassource: true,
-      islive: true,
-      technology: [
-        "nextjs",
-        "nextauth",
-        "prisma",
-        "shadcn",
-        "mdx",
-        "webscrapping",
-        "restapi",
-      ],
-      ongoing: false,
-      img: "",
-    },
-    {
-      name: "HashBlog",
-      description:
-        "A blog application using React for the frontend and MongoDB for data storage. Integrated Quill for rich text editing, Cloudflare for security and performance, Hono for the backend, and Cloudinary for image management. ",
-      source: "",
-      live: "",
-      start: "",
-      end: "",
-      hassource: true,
-      islive: true,
-      technology: [
-        "react",
-        "mongodb",
-        "quill",
-        "cloudflare",
-        "hono",
-        "cloudinary",
-      ],
-      ongoing: false,
-      img: "",
-    },
     {
       name: "Helper.ai",
       description:
@@ -84,7 +31,64 @@ function Work() {
         "geminiai",
       ],
       ongoing: true,
-      img: "",
+      img: one,
+    },
+    {
+      name: "Cs One Stop",
+      description:
+        "Optimized database models for 1000+ records using PrismaORM. Deployed NextAuth for authentication and added a portfolio interface. Integrated Judge0 IDE for real-time code execution, enhancing learning for 100+ students. Engineered a web scraping solution for job notifications.",
+      source: "",
+      live: "",
+      start: "",
+      end: "",
+      hassource: true,
+      islive: true,
+      technology: [
+        "nextjs",
+        "nextauth",
+        "prisma",
+        "shadcn",
+        "mdx",
+        "webscrapping",
+        "restapi",
+      ],
+      ongoing: true,
+      img: two,
+    },
+    {
+      name: "GuruCodes",
+      description:
+        "A high-converting course landing page, attracting over 100 daily visitors and facilitating 300+ course sales. Integrated Razorpay for secure transactions, directly contributing to revenue generation. Deployed and managed the application on AWS EC2, enhancing brand consistency and user trust.",
+      source: "",
+      live: "",
+      hassource: true,
+      islive: true,
+      start: "",
+      end: "",
+      technology: ["nextjs", "razorpay", "magicui", "tailwind", "analytics"],
+      ongoing: false,
+      img: three,
+    },
+    {
+      name: "HashBlog",
+      description:
+        "A blog application using React for the frontend and MongoDB for data storage. Integrated Quill for rich text editing, Cloudflare for security and performance, Hono for the backend, and Cloudinary for image management. ",
+      source: "",
+      live: "",
+      start: "",
+      end: "",
+      hassource: true,
+      islive: true,
+      technology: [
+        "react",
+        "mongodb",
+        "quill",
+        "cloudflare",
+        "hono",
+        "cloudinary",
+      ],
+      ongoing: false,
+      img: four,
     },
   ]);
   return (
@@ -95,19 +99,22 @@ function Work() {
             className="border rounded-2xl cursor-pointer hover:shadow-lg transition-all ease-in-out max-w-80"
             key={i}
           >
-            <Image
-              src={dummy}
-              width={280}
-              height={100}
-              placeholder="blur"
-              alt="work"
-              className="rounded-t-2xl"
-            />
+            <div className="flex justify-center p-5">
+              <Image
+                src={project.img}
+                width={100}
+                height={20}
+                placeholder="blur"
+                alt="work"
+                className="rounded-t-2xl"
+              />
+            </div>
             <div className="p-3">
-              <div>
-                <p className="text-sm font-bold">{project.name}</p>
+              <div className="flex gap-1 items-center">
+                <span className="text-sm font-bold">{project.name}</span>
+                {project.ongoing ? <span class="blink_me"></span> : <></>}
               </div>
-              <div className="justify-content h-32 text-xs/[14px] py-2">
+              <div className="justify-content h-32 text-xs/[14px] py-2 text-slate-500 dark:text-slate-400 ">
                 {project.description}
               </div>
               <div className="flex items-center gap-1 flex-wrap">
