1f86b871dbfb07d56bc7a72b0e8bd68dd757934c
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-09-18 13:18:32 +0530

    updated navabar

--- app/(with-footer)/works/_components/GetYears.jsx
+++ app/(with-footer)/works/_components/GetYears.jsx
@@ -1,39 +1,50 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import GitHubCalendar from "react-github-calendar";
+import { isMobile } from "react-device-detect";
+import { useTheme } from "next-themes";
 
 function GetYears() {
-  const years = [2022, 2023, 2024];
+  const years = [2022, 2023, 2024, 1];
   const [yearState, setYearState] = useState(0);
+  const { theme, setTheme } = useTheme();
 
   const handleYearChange = (year) => {
-    console.log(year);
     setYearState(year);
   };
+
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Suspense fallback="Loadin...">
-        {yearState === 0 ? (
-          <GitHubCalendar username="KiranPolaki" blockSize={8} />
-        ) : (
-          <GitHubCalendar
-            username="KiranPolaki"
-            blockSize={8}
-            year={yearState}
-          />
-        )}
+        <>
+          {yearState === 0 || yearState === 1 ? (
+            <GitHubCalendar
+              username="KiranPolaki"
+              blockSize={!isMobile ? 8 : 4}
+              colorScheme={theme === "dark" ? "dark" : "light"}
+            />
+          ) : (
+            <GitHubCalendar
+              username="KiranPolaki"
+              blockSize={!isMobile ? 8 : 4}
+              year={yearState}
+              colorScheme={theme === "dark" ? "dark" : "light"}
+            />
+          )}
+        </>
       </Suspense>
       <div className="flex gap-5">
         {years.map((year) => {
           return (
             <Button
               variant="outline"
+              className="shadow-md"
               key={year}
               onClick={() => handleYearChange(year)}
             >
-              {year}
+              {year != 1 ? year : "recet"}
             </Button>
           );
         })}
